71ca72930fd7c0959a0b16214e3f8ffa11af5873
matt5g <matthewg123798@gmail.com>
2025-01-10 22:48:32 -0500

    variable rgb color speed

--- RanGB.js
+++ RanGB.js
@@ -1,10 +1,10 @@
 let colorVal
-// elementID and eType (element type) are both inputted as strings, e.g, randomRGB("titleFont","weight")
-function randomRGB(elementID,eType) {
+// elementID and eType (element type) are both inputted as strings, tick is the time in ms between each color frame and should be an integer, e.g, randomRGB("titleFont","weight",10)
+function randomRGB(elementID,eType,tick) {
 setInterval(function () {
     colorVal = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"
     document.getElementById(elementID).style[eType] = colorVal
     //console.log(document.getElementById(elementID).style.eType);
     
-}, 2);
+}, tick);
 };
